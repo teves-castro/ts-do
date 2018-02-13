bbc20c86cdf1ff74dd7a7c206aa483c4b83555c2
Vítor Castro <vcastro70@gmail.com>
2018-02-13 19:10:13 +0000

    cleanup

--- src/builders.ts
+++ src/builders.ts
@@ -1,5 +1,4 @@
-import { Type, Type2, URIS, URIS2 } from "fp-ts/lib/HKT"
-import { HKT } from "fp-ts/lib/HKT"
+import { HKT, Type, Type2, URIS, URIS2 } from "fp-ts/lib/HKT"
 import { Monad } from "fp-ts/lib/Monad"
 import { sequence, Traversable } from "fp-ts/lib/Traversable"
 
