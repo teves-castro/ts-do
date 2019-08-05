563b619667d2c5fd751ed360313aa0bf003c6f1d
Vitor Castro <vcastro@equalexperts.com>
2019-08-05 18:02:08 +0100

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,38 @@ import { Kind, Kind2, Kind3, Kind4, URIS, URIS2, URIS3, URIS4 } from "fp-ts/lib/
 import { Monad1, Monad2, Monad3, Monad4 } from "fp-ts/lib/Monad"
 import { Traversable1 } from "fp-ts/lib/Traversable"
 
-type Kleisli<M extends URIS, A, B> = (a: A) => Kind<M, B>
-type Kleisli2<M extends URIS2, L, A, B> = (a: A) => Kind2<M, L, B>
-type Kleisli3<M extends URIS3, E, L, A, B> = (a: A) => Kind3<M, E, L, B>
-type Kleisli4<M extends URIS4, S, E, L, A, B> = (a: A) => Kind4<M, S, E, L, B>
+type K1<M extends URIS, A, B> = (a: A) => Kind<M, B>
+type K2<M extends URIS2, L, A, B> = (a: A) => Kind2<M, L, B>
+type K3<M extends URIS3, E, L, A, B> = (a: A) => Kind3<M, E, L, B>
+type K4<M extends URIS4, S, E, L, A, B> = (a: A) => Kind4<M, S, E, L, B>
 
 // Bind
 export function bind<M extends URIS4>(
   M: Monad4<M>,
 ): <S, E, L, A, B, N extends string>(
   n: N,
-  fb: Kleisli4<M, S, E, L, A, B>,
+  fb: K4<M, S, E, L, A, B>,
 ) => (fa: Kind4<M, S, E, L, A>) => Kind4<M, S, E, L, A & { [K in N]: B }>
 
 export function bind<M extends URIS3>(
   M: Monad3<M>,
 ): <E, L, A, B, N extends string>(
   n: N,
-  fb: Kleisli3<M, E, L, A, B>,
+  fb: K3<M, E, L, A, B>,
 ) => (fa: Kind3<M, E, L, A>) => Kind3<M, E, L, A & { [K in N]: B }>
 
 export function bind<M extends URIS2>(
   M: Monad2<M>,
 ): <L, A, B, N extends string>(
   n: N,
-  fb: Kleisli2<M, L, A, B>,
+  fb: K2<M, L, A, B>,
 ) => (fa: Kind2<M, L, A>) => Kind2<M, L, A & { [K in N]: B }>
 
 export function bind<M extends URIS>(
   M: Monad1<M>,
 ): <A, B, N extends string>(
   n: N,
-  fb: Kleisli<M, A, B>,
+  fb: K1<M, A, B>,
 ) => (fa: Kind<M, A>) => Kind<M, A & { [K in N]: B }>
 
 export function bind<M extends URIS>(M: Monad1<M>) {
@@ -71,25 +71,23 @@ export function into<M extends URIS>(M: Monad1<M>) {
 export function exec<M extends URIS4>(
   M: Monad4<M>,
 ): <S, E, L, A>(
-  fb: Kleisli4<M, S, E, L, A, unknown>,
+  fb: K4<M, S, E, L, A, unknown>,
 ) => (fa: Kind4<M, S, E, L, A>) => Kind4<M, S, E, L, A>
 
 export function exec<M extends URIS3>(
   M: Monad3<M>,
-): <E, L, A>(
-  fb: Kleisli3<M, E, L, A, unknown>,
-) => (fa: Kind3<M, E, L, A>) => Kind3<M, E, L, A>
+): <E, L, A>(fb: K3<M, E, L, A, unknown>) => (fa: Kind3<M, E, L, A>) => Kind3<M, E, L, A>
 
 export function exec<M extends URIS2>(
   M: Monad2<M>,
-): <L, A>(fb: Kleisli2<M, L, A, unknown>) => (fa: Kind2<M, L, A>) => Kind2<M, L, A>
+): <L, A>(fb: K2<M, L, A, unknown>) => (fa: Kind2<M, L, A>) => Kind2<M, L, A>
 
 export function exec<M extends URIS>(
   M: Monad1<M>,
-): <A>(fb: Kleisli<M, A, unknown>) => (fa: Kind<M, A>) => Kind<M, A>
+): <A>(fb: K1<M, A, unknown>) => (fa: Kind<M, A>) => Kind<M, A>
 
 export function exec<M extends URIS>(M: Monad1<M>) {
-  return <A>(fb: Kleisli<M, A, unknown>) => (fa: Kind<M, A>) =>
+  return <A>(fb: K1<M, A, unknown>) => (fa: Kind<M, A>) =>
     M.chain(fa, a => M.map(fb(a), () => a))
 }
 
@@ -99,7 +97,7 @@ export function sequence<T extends URIS, M extends URIS4>(
   M: Monad4<M>,
 ): <S, E, L, A, B, N extends string>(
   n: N,
-  fb: Kleisli<T, A, Kind4<M, S, E, L, B>>,
+  fb: K1<T, A, Kind4<M, S, E, L, B>>,
 ) => (fa: Kind4<M, S, E, L, A>) => Kind4<M, S, E, L, A & { [K in N]: Kind<T, B> }>
 
 export function sequence<T extends URIS, M extends URIS3>(
@@ -107,7 +105,7 @@ export function sequence<T extends URIS, M extends URIS3>(
   M: Monad3<M>,
 ): <E, L, A, B, N extends string>(
   n: N,
-  fb: Kleisli<T, A, Kind3<M, E, L, B>>,
+  fb: K1<T, A, Kind3<M, E, L, B>>,
 ) => (fa: Kind3<M, E, L, A>) => Kind3<M, E, L, A & { [K in N]: Kind<T, B> }>
 
 export function sequence<T extends URIS, M extends URIS2>(
@@ -115,7 +113,7 @@ export function sequence<T extends URIS, M extends URIS2>(
   M: Monad2<M>,
 ): <L, A, B, N extends string>(
   n: N,
-  fb: Kleisli<T, A, Kind2<M, L, B>>,
+  fb: K1<T, A, Kind2<M, L, B>>,
 ) => (fa: Kind2<M, L, A>) => Kind2<M, L, A & { [K in N]: Kind<T, B> }>
 
 export function sequence<T extends URIS, M extends URIS>(
@@ -123,15 +121,13 @@ export function sequence<T extends URIS, M extends URIS>(
   M: Monad1<M>,
 ): <A, B, N extends string>(
   n: N,
-  fb: Kleisli<T, A, Kind<M, B>>,
+  fb: K1<T, A, Kind<M, B>>,
 ) => (fa: Kind<M, A>) => Kind<M, A & { [K in N]: Kind<T, B> }>
 
 export function sequence<T extends URIS, M extends URIS>(
   T: Traversable1<T>,
   M: Monad1<M>,
 ) {
-  return <A, B, N extends string>(n: N, fb: Kleisli<T, A, Kind<M, B>>) => (
-    fa: Kind<M, A>,
-  ) =>
+  return <A, B, N extends string>(n: N, fb: K1<T, A, Kind<M, B>>) => (fa: Kind<M, A>) =>
     M.chain(fa, a => M.map(T.sequence(M)(fb(a)), v => Object.assign({}, a, { [n]: v })))
 }
